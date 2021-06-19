e7d9405fceaa4878ded7073f66fbc2396bb73aa6
Ivan Mendez <ivanmr932@gmail.com>
2021-06-18 20:34:27 -0400

    create selec component

---
+++ src/components/Select.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-select label="Paises" :items="paises"></v-select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Select',
+  data: () => ({
+    paises: ['Venezuela', 'EEUE', 'RUSIA']
+  })
+}
+</script>
+
+<style></style>
