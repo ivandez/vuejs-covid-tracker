399becc121df9674db041f1e90987ee50656b50c
Ivan Mendez <ivanmr932@gmail.com>
2021-06-29 16:40:45 -0400

    deploy settings

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,25 @@
-#!/usr/bin/env sj
+#!/usr/bin/env sh
 
+# abort on errors
 set -e
 
+# build
 npm run build
 
+# navigate into the build output directory
 cd dist
 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
 git add -A
+git commit -m 'deploy'
 
-git commit -m "new deployment"
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-git push -f git@github.com:ivandez/vuejs-covid-tracker.git
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:<ivandez>/<vuejs-covid-tracker>.git main:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: '/vuejs-covid-tracker/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vuejs-covid-tracker/'
+    : '/'
 }
